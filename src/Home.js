47279b87591c216c8b46da0985091e365ea8e4c3
mustafaTokmak <mtokmak06@gmail.com>
2021-06-04 02:38:50 +0300

    useState hook added

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,23 @@
+import { useState } from 'react'; 
 const Home = () => {
+    // let name = 'mario';
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(25);
+
     const handleClick = (e) => {
+        setName('luigi');
+        setAge(23)
         console.log('Hello From Ninja React Course',e.target)
+        console.log(name);
     }
     const handleClickAgain = (name) => {
         console.log('hello ' + name);
 
     }
     return (
-        <div className="home">
+        <div className="home"> 
             <h2>HomePage</h2>
+            <p>{ name } is { age } years old.</p>
             <button onClick={handleClick}>Click me</button>
             <button onClick={() => {
                 console.log('hello from inline func')
