6d18e88985310dd8470b7322732f923d8e0bd5a2
mustafaTokmak <mtokmak06@gmail.com>
2021-06-04 01:52:57 +0300

    add style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar'
+import Home from './Home'
 
 function App() {
+  const title = 'Welcome to the new blog';
+  const likes = 50;
+  const person = {name:'must', age:27 };
+  const link = "http://www.google.com"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Home/>
+
+      <div className="content">
+        <h1>{ title }</h1>
+        <p>Liked {likes} times</p>
+        {/* <p>{person}</p> */}
+        <p>{10}</p>
+        <p>{"hello, to mars"}</p>
+        <p>{[1,2,3,4,5,6]}</p>
+        <p>{Math.random() * 10}</p>
+        <a href={link}>Google Site</a>
+      </div>
+      
+  
     </div>
   );
 }
